33e0397e9314a7f0b31f71ab5496289403af079f
VarshiniManokaran <varshini6678@gmail.com>
2024-08-06 16:58:54 +0530

    Add files via upload

---
+++ Assignment4.java
@@ -0,0 +1,30 @@
+public class Assignment4 {
+    public static void main(String[] args) {
+        int a=30;
+        a += 500;  
+        System.out.println( "a is:"+a); 
+
+        int abc=400;
+        abc -= 600;
+        System.out.println("abc is:"+abc);
+
+
+        int number=7865;
+        number*= 30;
+        System.out.println("number is:"+number);
+
+
+
+        int number1=6678;
+        number1/= 12;
+        System.out.println("number1:"+number1);
+
+
+
+
+        
+
+
+
+    }
+}
